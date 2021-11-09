6f45b2d606c7dd25ab2824cc97631f9c95a5b461
Raúl García-Verdugo Cardador <60638702+Zerronir@users.noreply.github.com>
2021-11-09 13:57:28 +0100

    crear el countdown

---
+++ js/countdown.js
@@ -0,0 +1,31 @@
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+let intervalController;
+let actualTime;
+let distance;
+
+const startCountdown = (deadLine, timeout) => {
+    intervalController = setInterval(() => {
+        actualTime = new Date().getTime(); // Inicializamos un nuevo objeto de tiempo
+        let distance = deadLine - actualTime; // Creamos el objeto de la diferencia en el programa para calcular la distancia hasta el itempo objetivo
+
+        // Calculamos los tiempos
+
+
+    }, typeof timeout !== "undefined" ? timeout : 1000); // Se actualiza cada segundo
+}
+
+const stopTheCountdown = () => {
+    clearInterval(intervalController);
+}
+
+const resetCountdown = () => {
+    stopTheCountdown();
+    actualTime = null;
+}
+
+const playDeadLineSound = () => {
+    let audio = new Audio("./resources/audio/countdownAlarm.mp3");
+    audio.play();
+}
